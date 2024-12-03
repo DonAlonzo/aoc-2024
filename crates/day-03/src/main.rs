aa4b750f7e2fb3225880bc8c93d9a7416c71d391
Emil Boman <emil@boman.io>
2024-12-03 21:30:01 +0100

    Add failing test

--- crates/day-03/src/main.rs
+++ crates/day-03/src/main.rs
@@ -1,4 +1,20 @@
 fn main() {}
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let instructions = parse(include_str!("../input/test"));
+        assert_eq!(
+            instructions,
+            vec![
+                Instruction(2, 4),
+                Instruction(5, 5),
+                Instruction(11, 8),
+                Instruction(8, 5),
+            ]
+        );
+    }
+}
