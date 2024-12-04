b5b98b0579e2446254c0b8e50206c8f1d0f0b4a3
Emil Boman <emil@boman.io>
2024-12-04 06:50:19 +0100

    fixup! Add failing tests for day 4 part 1

--- crates/day-04/src/bin/part1.rs
+++ crates/day-04/src/bin/part1.rs
@@ -1,5 +1,8 @@
 #[derive(Debug, PartialEq)]
-struct WordSearch;
+struct WordSearch<'a> {
+    letters: &'a [char],
+    width: usize,
+}
 
 fn main() {}
 
@@ -19,7 +22,21 @@ mod tests {
     fn test_parse() {
         let input = include_str!("../../input/test");
         let word_search = parse(input);
-        assert_eq!(word_search, WordSearch);
+        assert_eq!(
+            word_search,
+            WordSearch {
+                letters: &[
+                    'M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M', 'M', 'S', 'A', 'M', 'X', 'M',
+                    'S', 'M', 'S', 'A', 'A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M', 'M', 'S',
+                    'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X', 'X', 'M', 'A', 'S', 'A', 'M', 'X', 'A',
+                    'M', 'M', 'X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A', 'S', 'M', 'S', 'M',
+                    'S', 'A', 'S', 'X', 'S', 'S', 'S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A',
+                    'M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M', 'M', 'X', 'M', 'X', 'A', 'X',
+                    'M', 'A', 'S', 'X',
+                ],
+                width: 10,
+            }
+        );
     }
 
     #[test]
