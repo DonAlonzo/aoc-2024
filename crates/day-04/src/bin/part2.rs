34c24651ba77df48ee99fbc7810d5f6117cf723a
Emil Boman <emil@boman.io>
2024-12-04 06:39:46 +0100

    Add day 4

---
+++ crates/day-04/src/bin/part2.rs
@@ -0,0 +1,20 @@
+fn main() {}
+
+fn parse(input: &str) {
+    todo!();
+}
+
+fn solve() {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {}
+
+    #[test]
+    fn test_solve() {}
+}
